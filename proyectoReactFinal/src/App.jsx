2ed5a566f5d4c7360b5af99e9393850fa7e91166
Lucia Bide <luciabide9@gmail.com>
2026-05-26 10:03:35 -0300

    Sacando el footer de App.jsx para hacer un componente Layout

--- proyectoReactFinal/src/App.jsx
+++ proyectoReactFinal/src/App.jsx
@@ -74,36 +74,6 @@ function App() {
         <Route path="/register" element={<Register/>}/>
         <Route path="/*" element={<NotFound/>}/>
       </Routes>
-      
-      <footer className="footer">
-        <div className="footer-container">
-          <div className="column-footer">
-            <h3 className="title-footer">PURE SKIN</h3>
-            <p className="text-footer">Comprometidos con resaltar tu armonía natural a través de ciencia y tecnología.</p>
-            <img src="/img/footerImg/instagram-ico.webp" alt="logo-instagram"/>
-          </div>
-
-          <div className="column-footer">
-            <h3 className="title-footer">EXPLORAR</h3>
-            <NavLink to="/Home">Inicio</NavLink>
-            <NavLink to="/Servicios">Servicios</NavLink>
-          </div>
-
-            <div className="column-footer">
-              <h3 className="title-footer">HORARIOS</h3>
-              <p className="text-footer">LUNES A VIERNES</p>
-              <p>09:00 a 20:00</p>
-              <p className="text-footer">SÁBADOS</p>
-              <p>09:00 a 14:00</p>
-            </div>
-
-            <div className="section-terminos">
-              <p>© 2026 PURE SKIN Rosario.</p>
-              <p>Privacidad y términos</p>
-            </div>
-        </div>
-      </footer>
-
     </HashRouter>
   </>);
 }
