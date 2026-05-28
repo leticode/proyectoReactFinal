f08a8f3ef7f76cd2dabe78b89a5ab89dac246e78
Lucia Bide <luciabide9@gmail.com>
2026-05-28 17:03:16 -0300

    Haciendo estructura y estilo de la página Home.jsx

--- proyectoReactFinal/src/components/home/Home.jsx
+++ proyectoReactFinal/src/components/home/Home.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import React from "react";
 
-const Home = () =>{
-    return(
+const Home = () => {
+    return (
         <>
-            <body className="home-body">
+            <main className="home-main">
                 <section className="home-section">
                     <div className="home-presentation">
                         <h2 className="home-title">Belleza, equilibrio y bienestar en un solo lugar</h2>
-                        <p className="home-text">Trabajamos con estándares profesionales, equipamiento moderno y un enfoque personalizado para garantizar seguridad, eficacia y resultados que potencian tu belleza natural.</p>
+                        <p className="home-text">
+                            Trabajamos con estándares profesionales,
+                            equipamiento moderno y un enfoque personalizado
+                            para garantizar seguridad, eficacia y resultados
+                            que potencian tu belleza natural.
+                        </p>
                     </div>
                     <div className="home-buttons">
-                        <button className="button-services">Servicios</button>
-                        <button className="button-us">Nosotros</button>
+                        <button className="btn-services">Nuestros servicios</button>
+                        <button className="btn-us">Sobre nosotros</button>
                     </div>
+                    
                 </section>
-            </body>
+            </main>
         </>
     )
 };
