985253ad5bbacb7368c878d5ff8b968443f7340b
Lucia Bide <luciabide9@gmail.com>
2026-06-02 13:20:54 -0300

    Haciendo la estructura para los datos de los turnos

---
+++ proyectoReact_Api/src/models/Appointment.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
+import User from "./User.js";
+import { Service } from "./Service.js";
+
+export const Appointment = sequelize.define(
+  "Appointment",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATEONLY
+    },
+    hour: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM(
+        "pending",
+        "confirmed",
+        "cancelled",
+        "completed"
+      ),
+      defaultValue: "pending",
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    serviceId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "appointment",
+  }
+);//FK preguntar a marcelo
