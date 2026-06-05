3553377ebbe873b443d2ff75201602f833f507f8
Lucia Bide <luciabide9@gmail.com>
2026-06-04 22:01:46 -0300

    Modificando la tabla profesionales

--- proyectoReact_Api/src/models/Assosiations.js
+++ proyectoReact_Api/src/models/Assosiations.js
@@ -9,14 +9,47 @@ Appointment.belongsTo(User, {
   as: "user",
 });
 
+User.hasMany(Appointment, {
+  foreignKey: "userId",
+});
+
 // Profesional
 Appointment.belongsTo(Professionals, {
   foreignKey: "professionalId",
   as: "professional",
 });
 
+Professionals.hasMany(Appointment, {
+  foreignKey: "professionalId",
+});
+
+// Profesional a user
+Professionals.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+User.hasOne(Professionals, {
+  foreignKey: "userId",
+});
+
 // Servicio
 Appointment.belongsTo(Service, {
   foreignKey: "serviceId",
   as: "service",
+});
+
+Service.hasMany(Appointment, {
+  foreignKey: "serviceId",
+});
+
+
+//Profesional a servicio
+Professionals.belongsTo(Service, {
+  foreignKey: "serviceId",
+  as: "service",
+});
+
+Service.hasMany(Professionals, {
+  foreignKey: "serviceId",
 });
