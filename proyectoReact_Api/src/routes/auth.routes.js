c886e5876742b49e51d73a080642d3dca002abd6
Lucia Romero <luciromee@gmail.com>
2026-06-04 21:53:21 -0300

    instale la libreria de jwt para generar el token, crea carpeta de middlewares para validar el token y los formularios.

--- proyectoReact_Api/src/routes/auth.routes.js
+++ proyectoReact_Api/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import {registerUser, loginUser } from "../services/auth.services.js"
+import { registerUser, loginUser } from "../services/auth.services.js"
+import { verifyLogin, verifyRegister } from "../middleware/auth.validations.js";
 
 //las rutas de autenticacion
 const router = Router();
 
-router.post("/login", loginUser);
-router.post("/register", registerUser);
+router.post("/login", verifyLogin, loginUser);
+router.post("/register", verifyRegister, registerUser);
 
 
 export default router;
