e8bdbd5322334527018261a84d4c9d6e37695335
Leticia <leticiasoulos@gmail.com>
2026-06-06 17:40:12 -0300

    cargo los servicios en el admin y por ahora muestro sus img, luego voy a hacer una tabla para el abm

--- proyectoReactFinal/src/components/admin/Admin.jsx
+++ proyectoReactFinal/src/components/admin/Admin.jsx
@@ -1,21 +1,45 @@
-import React from "react";
-import {useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
 
 const Admin = () => {
+    const [serverMessage, setServerMessage] = useState("");
+    const [services, setServices] = useState([]);
 
-    const navigate = useNavigate();
+    useEffect(() => {
+        const loadServices = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/api/services", {
+                    method: "GET",
+                });
+                if (!response.ok) {
+                    setServerMessage(data.message || "error al traer servicios");
+                    return;
+                }
 
-    const handleBackToHome = () => {
-        navigate("/home"); 
-    }
+                const data = await response.json();
+                setServices(data);
+            } catch (error) {
+                console.error(error);
+                setServerMessage("Error al conectar con el servidor");
+            }
+        };
+
+        loadServices();
+    }, []);
+
 
     return (
         <div className="notFound-container">
             <div className="notFound-box">
                 <h2 className="notFound-title">Admin</h2>
-                <button className="notFound-btn" onClick={handleBackToHome}>
-                    Volver al inicio
-                </button>
+                {serverMessage && (<p className="server-message">{serverMessage}</p>)}
+                {services.map(service => (
+                <div className="card" key={service.id}>
+                    <div className="img-container">
+                    <img src={service.img} alt={service.name} />
+                    <p className="titulo">{service.name}</p>
+                    </div>
+                </div>
+                ))}
             </div>
         </div>
     )
