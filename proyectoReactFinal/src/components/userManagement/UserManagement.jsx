15b738333204ffdd5da50281e20020fd0d73e98e
Leticia <leticiasoulos@gmail.com>
2026-06-10 16:29:42 -0300

    instale dependecias de la libreria "tokenValid" y agregue a la exportacion el ".js" ya que no funcionaba

--- proyectoReactFinal/src/components/userManagement/UserManagement.jsx
+++ proyectoReactFinal/src/components/userManagement/UserManagement.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "../services/auth/authContextProvider";
 import ValidateUserManagement from "../../utils/validateUserManagement";
-import tokenValid from "../services/auth/auth.token";
+import tokenValid from "../services/auth/auth.token.js";
 
 const UserManagement = () => {
     const { token, handleUserLogout } = useContext(AuthenticationContext);
@@ -33,9 +33,9 @@ const UserManagement = () => {
             [name]: value
         });
     }
-    
+
     //permte ejecutar el codgo cuando se renderza el componente
-    useEffect (() => {
+    useEffect(() => {
         //el token es valdo? si no lo es lo desloguea borra desde el localstorage y navega al login
         if (!tokenValid(token)) {
             handleLogoutUser();
@@ -48,24 +48,24 @@ const UserManagement = () => {
             headers: { Authorization: `Bearer ${token}` },
         })
 
-        //el front recibe la respuesta del backend PERO solo recibe la response status
-        //osea solo recibe o 201 o 401 no el objeto de usuario 
-        .then((res) => {
-            //si el error empieza con 400 algo tira error 
-            if (!res.ok) throw new Error("Error al cargar usuarios");
+            //el front recibe la respuesta del backend PERO solo recibe la response status
+            //osea solo recibe o 201 o 401 no el objeto de usuario 
+            .then((res) => {
+                //si el error empieza con 400 algo tira error 
+                if (!res.ok) throw new Error("Error al cargar usuarios");
 
-            //si todo sale bien convierte la response a objeto
-            return res.json();
-        })
+                //si todo sale bien convierte la response a objeto
+                return res.json();
+            })
 
-        //ahora si este then tiene el objeto usuario
-        .then((data) => {
-            //y actualiza el estado de users con el objeto recibido
-            setUsers(data);
-        })
+            //ahora si este then tiene el objeto usuario
+            .then((data) => {
+                //y actualiza el estado de users con el objeto recibido
+                setUsers(data);
+            })
 
-        .catch(() => setServerMessage("No se pudieron cargar los usuarios"));
-  
+            .catch(() => setServerMessage("No se pudieron cargar los usuarios"));
+
     }, []);
 
     const handleCreateUser = async (event) => {
@@ -98,96 +98,96 @@ const UserManagement = () => {
             body: JSON.stringify(formUser),
         })
 
-        .then((res) => {
-				if (!res.ok) {
+            .then((res) => {
+                if (!res.ok) {
                     //corta la ejecucon throw
-					throw new Error('No se pudieron cargar los usuarios');
-				}
+                    throw new Error('No se pudieron cargar los usuarios');
+                }
 
-				return res.json();
-			})
-		.then(() => {
-            //aca limpiamos los inputs desp de crear el usuario
-            setformUser({
-                email: "",
-                password: "",
-                confirmPassword: "",
-                role: "customer",
+                return res.json();
+            })
+            .then(() => {
+                //aca limpiamos los inputs desp de crear el usuario
+                setformUser({
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                    role: "customer",
+                });
+
+                setServerMessage("Usuario creado correctamente");
+            })
+            .catch((error) => {
+                console.log(error);
+                setServerMessage('No se pudo crear al usuario');
             });
-
-            setServerMessage("Usuario creado correctamente");
-        })
-		.catch((error) => {
-			console.log(error);
-			setServerMessage('No se pudo crear al usuario');
-		});
     }
 
     return (
         <section className="management">
-                <div className="management-container">
+            <div className="management-container">
 
-                        <form onSubmit={handleCreateUser}>
-                            <h1>Gestion de Usuarios</h1>
+                <form onSubmit={handleCreateUser}>
+                    <h1>Gestion de Usuarios</h1>
 
-                            <div className="input-container">
-                                <label>Email</label>
-                                <input className="management-input"
-                                    name = "email"
-                                    ref={emailRef}
-                                    type="email"
-                                    placeholder="Ingresar Email"
-                                    value={formUser.email}
-                                    onChange={handleChange}
-                                />
-                                {errors.email && <p className="errors" >El email ingresado debe ser válido.</p>}
-                            </div>    
+                    <div className="input-container">
+                        <label>Email</label>
+                        <input className="management-input"
+                            name="email"
+                            ref={emailRef}
+                            type="email"
+                            placeholder="Ingresar Email"
+                            value={formUser.email}
+                            onChange={handleChange}
+                        />
+                        {errors.email && <p className="errors" >El email ingresado debe ser válido.</p>}
+                    </div>
 
-                            <div className="input-container">
-                                <label>Contraseña</label>
-                                <input className="management-input"
-                                    name= "password"
-                                    ref={passwordRef}
-                                    type="password"
-                                    placeholder="Ingresar Contraseña"
-                                    value={formUser.password}
-                                    onChange={handleChange}
-                                />
-                                {errors.password && <p className="errors" >La contraseña debe tener al menos 7 caracteres y un caracter especial.</p>}
-                            </div>
+                    <div className="input-container">
+                        <label>Contraseña</label>
+                        <input className="management-input"
+                            name="password"
+                            ref={passwordRef}
+                            type="password"
+                            placeholder="Ingresar Contraseña"
+                            value={formUser.password}
+                            onChange={handleChange}
+                        />
+                        {errors.password && <p className="errors" >La contraseña debe tener al menos 7 caracteres y un caracter especial.</p>}
+                    </div>
 
-                            <div className="input-container">
-                            <label>Confirmar Contraseña</label>
-                                <input className="management-input"
-                                    name= "confirmPassword"
-                                    ref={confirmPasswordRef}
-                                    type="password"
-                                    placeholder="Ingresar Contraseña"
-                                    value={formUser.confirmPassword}
-                                    onChange={handleChange}
-                                />
-                                {errors.confirmPassword && <p className="errors" >La contraseña debe ser igual</p>}
-                                
-                            </div>
-                            <div className="input-container">
-                                <label>Rol de usuario</label>
-                                    <select
-                                        name="role"
-                                        value={formUser.role}
-                                        onChange={handleChange}
-                                    >
-                                        <option value="customer">Customer</option>
-                                        <option value="professional">Professional</option>
-                                        <option value="admin">Admin</option>
-                                    </select>
-                                
-                            </div>
-                            <button type = "submit"> Agregar </button>
-                            
-                            {serverMessage && (<p className="server-message">{serverMessage}</p>)}
-                        </form>
-                </div>
-            </section>
+                    <div className="input-container">
+                        <label>Confirmar Contraseña</label>
+                        <input className="management-input"
+                            name="confirmPassword"
+                            ref={confirmPasswordRef}
+                            type="password"
+                            placeholder="Ingresar Contraseña"
+                            value={formUser.confirmPassword}
+                            onChange={handleChange}
+                        />
+                        {errors.confirmPassword && <p className="errors" >La contraseña debe ser igual</p>}
+
+                    </div>
+                    <div className="input-container">
+                        <label>Rol de usuario</label>
+                        <select
+                            name="role"
+                            value={formUser.role}
+                            onChange={handleChange}
+                        >
+                            <option value="customer">Customer</option>
+                            <option value="professional">Professional</option>
+                            <option value="admin">Admin</option>
+                        </select>
+
+                    </div>
+                    <button type="submit"> Agregar </button>
+
+                    {serverMessage && (<p className="server-message">{serverMessage}</p>)}
+                </form>
+            </div>
+        </section>
     );
 }
 
