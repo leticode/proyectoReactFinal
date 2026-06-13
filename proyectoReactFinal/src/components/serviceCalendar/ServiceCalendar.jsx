5fc4dc329bf6279b200d5689a7feecd17bb33a32
Lucia Bide <luciabide9@gmail.com>
2026-06-13 19:34:07 -0300

    Creando el componente para el modal del calendario e instalando su libreria

---
+++ proyectoReactFinal/src/components/serviceCalendar/ServiceCalendar.jsx
@@ -0,0 +1,16 @@
+//use la libreria npm install react-calendar para hacer todo el visual del calendario
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+function ServiceCalendar() {
+
+    return (
+        <>
+            <div>
+                Calendario
+            </div>
+        </>
+    );
+}
+
+export default ServiceCalendar;
