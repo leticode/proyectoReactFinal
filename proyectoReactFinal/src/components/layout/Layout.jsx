97436d55b9335402793695362ee01ae179ca0f92
Lucia Romero <luciromee@gmail.com>
2026-06-15 16:55:15 -0300

    arreglos de app y layout

--- proyectoReactFinal/src/components/layout/Layout.jsx
+++ proyectoReactFinal/src/components/layout/Layout.jsx
@@ -100,7 +100,7 @@ const Layout = ({ children }) => {
           {(user?.role === "admin" || user?.role === "professional") && (
             <>
               <NavLink to="/admin" className={linkClass}>Admin</NavLink>
-              <NavLink to="/management" className={linkClass}>Gestion Usuarios</NavLink>
+              <NavLink to="/management" className={linkClass}>Gestion de Usuarios</NavLink>
             </>
           )}
 
