fbefe4c2ab4d7e9a5372948ed7024a4962f4b711
Leticia <leticiasoulos@gmail.com>
2026-06-15 14:18:59 -0300

    Revert "sincronizo archivos eliminados."
    This reverts commit 7c3cbe6675155bb03e61a39865981b32efc374a0.

---
+++ proyectoReactFinal/src/components/layout/Layout.jsx
@@ -0,0 +1,179 @@
+import { useState, useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AuthenticationContext } from "../services/auth/authContextProvider";
+import '../../index.css';
+
+const Layout = ({ children }) => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { handleUserLogout, user } = useContext(AuthenticationContext);
+  const [open, setOpen]= useState(false);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    handleUserLogout()
+    setOpen(false)
+    navigate("/home")
+  };
+  const login = () => {
+    setOpen(false)
+    navigate("/login")
+  };
+
+  const linkClass = ({ isActive }) =>
+    isActive ? "link active" : "link";
+
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+
+        {/* LOGO */}
+        <NavLink to="/home" className="logo">
+          PURE SKIN
+        </NavLink>
+
+        {/* NAV DESKTOP */}
+        <nav className="nav navbar">
+          <NavLink to="/admin" className={linkClass}>Admin</NavLink>
+
+          <NavLink to="/professionals" className={linkClass}>Profesionals</NavLink>
+
+          <NavLink to="/home" className={linkClass}>
+            Inicio
+          </NavLink>
+
+          <NavLink to="/servicios" className={linkClass}>
+            Servicios
+          </NavLink>
+
+          <NavLink to="/nosotros" className={linkClass}>
+            Nosotros
+          </NavLink>
+
+          <NavLink to="/contacto" className={linkClass}>
+            Contacto
+          </NavLink>
+
+          <div className="user-menu">
+              <img src="/img/navbarImg/user-icon.webp" alt="Img navbar" className = "imgLogin" onClick={() => setOpen(!open)}/>
+                
+                {open && (
+                <div className="dropdown">
+                  {user ? (
+                    <button onClick={logout}>
+                          Cerrar sesion
+                      </button>
+                  ) : (
+                    <button onClick={login}>
+                          Iniciar Sesion
+                      </button>
+                  )}
+                </div>
+                )}
+          </div>
+
+        </nav>
+
+        {/* HAMBURGUESA */}
+        <button
+          className="menu-btn"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span className={`menu-line ${menuOpen ? "open" : ""}`}></span>
+          <span className={`menu-line ${menuOpen ? "open" : ""}`}></span>
+          <span className={`menu-line ${menuOpen ? "open" : ""}`}></span>
+        </button>
+
+      </header>
+
+      {/* MENU MOBILE */}
+      {menuOpen && (
+        <div className="mobile-menu">
+
+          <NavLink to="/admin" className={linkClass}>Admin</NavLink>
+
+          <NavLink to="/home" onClick={() => setMenuOpen(false)} className={linkClass}>
+            Inicio
+          </NavLink>
+
+          <NavLink to="/servicios" onClick={() => setMenuOpen(false)} className={linkClass}>
+            Servicios
+          </NavLink>
+
+          <NavLink to="/nosotros" onClick={() => setMenuOpen(false)} className={linkClass}>
+            Nosotros
+          </NavLink>
+
+          <NavLink to="/contacto" onClick={() => setMenuOpen(false)} className={linkClass}>
+            Contacto
+          </NavLink>
+
+          {user ? (
+            <button
+              className="mobile-menu-btn"
+              onClick={() => {
+                logout();
+                setMenuOpen(false);
+              }}
+            >
+              Cerrar sesion
+            </button>
+          ) : (
+            <NavLink
+              to="/login"
+              onClick={() => setMenuOpen(false)}
+              className={linkClass}
+            >
+              Iniciar sesion
+            </NavLink>
+          )}
+
+        </div>
+      )}
+
+      {/* CONTENIDO */}
+      <main>{children}</main>
+
+      {/* FOOTER */}
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-top">
+
+            <div className="column-footer">
+              <h3 className="title-footer-PS">PURE SKIN</h3>
+              <p className="text-footer">
+                Comprometidos con resaltar tu armonía natural.
+              </p>
+            </div>
+
+            <div className="column-footer">
+              <h3 className="title-footer">EXPLORAR</h3>
+              <NavLink to="/home" className="link-footer">Inicio</NavLink>
+              <NavLink to="/servicios" className="link-footer">Servicios</NavLink>
+              <NavLink to="/nosotros" className="link-footer">Nosotros</NavLink>
+              <NavLink to="/contacto" className="link-footer">Contacto</NavLink>
+            </div>
+
+            <div className="column-footer">
+              <h3 className="title-footer">HORARIOS</h3>
+              <p className="subtitle-footer">Lunes a Viernes</p>
+              <p className="text-footer">09:00 a 20:00</p>
+              <p className="subtitle-footer">Sábados</p>
+              <p className="text-footer">09:00 a 14:00</p>
+            </div>
+
+          </div>
+
+          <div className="footer-bottom">
+            <p className="text-footer">© 2026 PURE SKIN Rosario</p>
+          </div>
+        </div>
+      </footer>
+
+    </>
+  );
+};
+
+export default Layout;
---
+++ proyectoReactFinal/src/components/userManagement/UserManagement.jsx
@@ -0,0 +1,235 @@
+import { useEffect, useState, useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthenticationContext } from "../services/auth/authContextProvider";
+import ValidateUserManagement from "../../utils/validateUserManagement";
+import tokenValid from "../services/auth/auth.token.js";
+
+const UserManagement = () => {
+    const { token, handleUserLogout, user } = useContext(AuthenticationContext);
+    const [users, setUsers] = useState([]);
+    const [formUser, setformUser] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        role: "customer"
+    });
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    const [serverMessage, setServerMessage] = useState("");
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const confirmPasswordRef = useRef(null);
+
+    //handle general que va guardardando todos los inputs a medida que el usuario escribe
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setformUser({
+            ...formUser,
+            [name]: value
+        });
+    }
+
+    //permte ejecutar el codgo cuando se renderza el componente
+    useEffect(() => {
+        //el token es valdo? si no lo es lo desloguea borra desde el localstorage y navega al login
+        if (!tokenValid(token)) {
+            handleLogoutUser();
+            navigate("/login");
+        }
+
+        //si es valido hace el la peticion para traer los usaruaios
+        fetch("http://localhost:3000/api/users", {
+            //mandamos como header el token
+            headers: { Authorization: `Bearer ${token}` },
+        })
+
+            //el front recibe la respuesta del backend PERO solo recibe la response status
+            //osea solo recibe o 201 o 401 no el objeto de usuario 
+            .then((res) => {
+                //si el error empieza con 400 algo tira error 
+                if (!res.ok) throw new Error("Error al cargar usuarios");
+
+                //si todo sale bien convierte la response a objeto
+                return res.json();
+            })
+
+            //ahora si este then tiene el objeto usuario
+            .then((data) => {
+                //y actualiza el estado de users con el objeto recibido
+                setUsers(data);
+            })
+
+            .catch(() => setServerMessage("No se pudieron cargar los usuarios"));
+
+    }, []);
+
+    const handleCreateUser = async (event) => {
+        //evitamos que recarguen la pagina
+        event.preventDefault();
+
+        const validationErrors = ValidateUserManagement(formUser);
+        //valdamos que el formularo este bien antes de hacer el fetch
+
+        //object.keys guarda las propiedades y con .lenght cuenta la longtud del array donde si es mayor a cero 
+        // es porq tiene errores 
+        if (Object.keys(validationErrors).length > 0) {
+
+            //aca se actualiza el estado con los errores validados
+            setErrors({
+                email: validationErrors.email || "",
+                password: validationErrors.password || "",
+                confirmPassword: validationErrors.confirmPassword || "",
+            })
+
+            return;
+        }
+
+        fetch("http://localhost:3000/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(formUser),
+        })
+
+            .then((res) => {
+                if (!res.ok) {
+                    //corta la ejecucon throw
+                    throw new Error('No se pudieron cargar los usuarios');
+                }
+
+                return res.json();
+            })
+            .then(() => {
+                //aca limpiamos los inputs desp de crear el usuario
+                setformUser({
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                    role: "customer",
+                });
+
+                setServerMessage("Usuario creado correctamente");
+            })
+            .catch((error) => {
+                console.log(error);
+                setServerMessage('No se pudo crear al usuario');
+            });
+    }
+
+    return (
+        <>
+        <h1>Gestion de Usuarios</h1>
+        <div className="management">
+            <div className="management-container">
+                {user?.role === "admin" && (
+                <form onSubmit={handleCreateUser} noValidate>
+
+                    <div className="input-container">
+                        <label>Email</label>
+                        <input className="management-input"
+                            name="email"
+                            ref={emailRef}
+                            type="email"
+                            placeholder="Ingresar Email"
+                            value={formUser.email}
+                            onChange={handleChange}
+                        />
+                        {errors.email && <p className="errors" >El email ingresado debe ser válido.</p>}
+                    </div>
+
+                    <div className="input-container">
+                        <label>Contraseña</label>
+                        <input className="management-input"
+                            name="password"
+                            ref={passwordRef}
+                            type="password"
+                            placeholder="Ingresar Contraseña"
+                            value={formUser.password}
+                            onChange={handleChange}
+                        />
+                        {errors.password && <p className="errors" >La contraseña debe tener al menos 7 caracteres y un caracter especial.</p>}
+                    </div>
+
+                    <div className="input-container">
+                        <label>Confirmar Contraseña</label>
+                        <input className="management-input"
+                            name="confirmPassword"
+                            ref={confirmPasswordRef}
+                            type="password"
+                            placeholder="Ingresar Contraseña"
+                            value={formUser.confirmPassword}
+                            onChange={handleChange}
+                        />
+                        {errors.confirmPassword && <p className="errors" >La contraseña debe ser igual</p>}
+
+                    </div>
+                    <div className="input-container">
+                        <label>Rol de usuario</label>
+                        <select
+                            name="role"
+                            value={formUser.role}
+                            onChange={handleChange}
+                        >
+                            <option value="customer">Customer</option>
+                            <option value="professional">Professional</option>
+                            <option value="admin">Admin</option>
+                        </select>
+
+                    </div>
+                    <button type="submit"> Agregar </button>
+
+                    {serverMessage && (<p className="server-message">{serverMessage}</p>)}
+                </form>
+                )}
+            </div>
+            
+            <div className="management-container">
+                <table>
+                    <thead> {/*encabezado*/}
+                        <tr> {/*row*/} 
+                            <th>Email</th>
+                            {user?.role === "admin" && (
+                                <>
+                                    <th>Rol</th>
+                                    <th></th>
+                                    <th></th>
+                                </>
+                            )}
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {/*mapeamos users por cada usuario con su id ceramos una fila*/}
+                        {users.map((u) => (
+                            <tr key={u.id}>
+                                <td>{u.email}</td>
+                                {user?.role === "admin" && (
+                                    <>
+                                        <td>{u.role}</td> {/*mostramos rol */}
+                                        <td> {/*botones para editar o borrar*/}
+                                            <button className="edit-button">Editar</button>
+                                        </td>
+                                        <td>
+                                            <button className="delete-button"> Eliminar</button>
+                                        </td>
+                                    </>
+                                )}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                {serverMessage && (<p className="server-message">{serverMessage}</p>)}
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default UserManagement;
