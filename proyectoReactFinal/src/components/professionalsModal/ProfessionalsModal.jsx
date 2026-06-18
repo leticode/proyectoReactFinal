e87553812c6a04cf1939294367c3a3f816718999
Lucia Bide <luciabide9@gmail.com>
2026-06-18 15:36:12 -0300

    Emepzando con el modal para profesionales

---
+++ proyectoReactFinal/src/components/professionalsModal/ProfessionalsModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const ProfessionalsModal = ({onClose, onSelect}) => {
+    const [professionals, setProfessionals] = useState([]);
+    return(
+        <>
+        <p>hola</p>
+        </>
+    )
+}
+
+export default ProfessionalsModal
