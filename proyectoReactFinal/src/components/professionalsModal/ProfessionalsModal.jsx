b7a2f0605e484c79f7f97b25793f490ac0c62380
Lucia Bide <luciabide9@gmail.com>
2026-06-18 15:49:41 -0300

    Siguiendo con el modal

--- proyectoReactFinal/src/components/professionalsModal/ProfessionalsModal.jsx
+++ proyectoReactFinal/src/components/professionalsModal/ProfessionalsModal.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { useState } from "react";
 
-const ProfessionalsModal = ({onClose, onSelect}) => {
+const ProfessionalsModal = ({ onClose, onSelect }) => {
     const [professionals, setProfessionals] = useState([]);
-    return(
+    //esto es para traer a los profesionales y asi poder mostrarlos
+    useEffect(() => {
+        fetch("http://localhost:3000/api/professionals")
+            .then((res) => res.json())
+            .then((data) => setProfessionals(data));
+    }, []);
+
+    return (
         <>
-        <p>hola</p>
+            <div className="modal-overlay">
+                <div className="modal-content">
+                    <h2>Selecciona un profesional</h2>
+
+                    {professionals.map((professional) => (
+                        <button
+                            key={professional.id}
+                            onClick={() => onSelect(professional)} //guarda un solo profesional
+                        >
+                            {professional.name}
+                        </button>
+                    ))}
+
+                    <button onClick={onClose}>
+                        Cerrar
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
