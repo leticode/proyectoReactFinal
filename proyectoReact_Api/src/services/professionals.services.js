54dbdc75f302788a24147cb5287c5ca0f7b5d769
Lucia Bide <luciabide9@gmail.com>
2026-06-21 02:07:34 -0300

    Agrego la funcion que devuelve los horarios disponibles de un profesional

--- proyectoReact_Api/src/services/professionals.services.js
+++ proyectoReact_Api/src/services/professionals.services.js
@@ -1,4 +1,31 @@
 import { Professionals } from "../models/Professionals.js";
+import { generateSlots } from "../services/appointment.services.js";
+
+export const getAvailableSlots = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const professional = await Professionals.findByPk(id);
+
+        if (!professional) {
+            return res.status(404).json({
+                message: "Profesional no encontrado"
+            });
+        }
+
+        const slots = generateSlots(
+            professional.workayStart,
+            professional.workayEnd,
+        );
+
+        res.json(slots);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
 
 export const getProfessionals = async (req, res) => {
     try {
